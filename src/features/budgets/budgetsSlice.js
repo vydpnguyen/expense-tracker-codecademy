4ee3f558be268ff2a950c27e354e19f1272a1dc2
Vy Nguyen <vydpnguyen@gmail.com>
2023-08-06 19:18:07 +0700

    Implement budgetsSlice

--- src/features/budgets/budgetsSlice.js
+++ src/features/budgets/budgetsSlice.js
@@ -1,27 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
 export const CATEGORIES = ['housing', 'food', 'transportation', 'utilities', 'clothing', 'healthcare', 'personal', 'education', 'entertainment'];
+
 const initialState = CATEGORIES.map(category => ({ category: category, amount: 0 }))
 
-export const editBudget = (budget) => {
-  return {
-    type: 'budgets/editBudget',
-    payload: budget
+const budgetsSlice = createSlice({
+  name: 'budgets',
+  initialState: initialState,
+  reducers: {
+    editBudget: (state, action) => {
+      const budgetToUpdate = state.find(budget => budget.category === action.payload.category);
+      budgetToUpdate.amount = action.payload.amount;
+    }
   }
-}
-
-const budgetsReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case 'budgets/editBudget':
-      const newBudgets = state.map(budget => {
-        if (budget.category === action.payload.category) {
-          return action.payload;
-        }
-        return budget;
-      })
-      return newBudgets;
-    default:
-      return state;
-  }
-}
+});
 
 export const selectBudgets = (state) => state.budgets;
-export default budgetsReducer;
+export const { editBudget } = budgetsSlice.actions;
+export default budgetsSlice.reducer;
+
+/*
+Object returned by createSlice have 3 properties: name, reducer and actions.
+*/
